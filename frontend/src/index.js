fca1274ee533af6f88eaeafa0a36340f414b65c8
Rushik Ramani <rush.ramani@gmail.com>
2022-08-16 13:59:33 -0700

    frontend user forms committed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,17 @@
-import ReactDOM  from 'react-dom/client';
-import App from './App';
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-const rootElement = document.getElementById('root');
+import "./index.css";
+import ThemeProvider from "./context/ThemeProvider";
+
+const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
 
-root.render(<App/>)
+root.render(
+  <BrowserRouter>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </BrowserRouter>
+);
